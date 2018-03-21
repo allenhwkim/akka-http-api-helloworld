9f889836a56912d518dd636d2a0243d0d63dbbb2
Allen Kim <allen.kim@rci.rogers.ca>
2018-03-21 11:33:30 -0400

    updated

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,71 +1,92 @@
-import akka.*;
-import akka.actor.*;
-import akka.http.javadsl.*;
-import akka.http.javadsl.marshallers.jackson.*;
-import akka.http.javadsl.model.*;
-import akka.http.javadsl.server.*;
-import akka.stream.*;
-import akka.stream.javadsl.*;
-import com.fasterxml.jackson.annotation.*;
-import java.util.*;
-import java.util.concurrent.*;
-// import static akka.http.javadsl.server.PathMatchers.longSegment;
+//package akka.http.javadsl.server.examples.petstore;
 
-public class HelloWorld extends AllDirectives {
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+//#imports
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
+//#imports
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
 
-    public static void main(String[] args) throws Exception {
-        // boot up server using the route as defined below
-        ActorSystem system = ActorSystem.create("routes");
+import java.io.IOException;
+//#imports
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+//#imports
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
-        final Http http = Http.get(system);
+//#imports
+import static akka.http.javadsl.server.Directives.*;
+import static akka.http.javadsl.unmarshalling.StringUnmarshallers.INTEGER;
+
+//#imports
+
+public class HelloWorld {
+
+    //#marshall
+    private static Route putPetHandler(Map<Integer, Pet> pets, Pet thePet) {
+        pets.put(thePet.getId(), thePet);
+        return complete(StatusCodes.OK, thePet, Jackson.<Pet>marshaller());
+    }
+
+    private static Route alternativeFuturePutPetHandler(Map<Integer, Pet> pets, Pet thePet) {
+        pets.put(thePet.getId(), thePet);
+        CompletableFuture<Pet> futurePet = CompletableFuture.supplyAsync(() -> thePet);
+        return completeOKWithFuture(futurePet, Jackson.<Pet>marshaller());
+    }
+    //#marshall
+
+    //#unmarshall
+    public static Route appRoute(final Map<Integer, Pet> pets) {
+        // PetStoreController controller = new PetStoreController(pets);
+
+        // Defined as Function in order to refer to [pets], but this could also be an ordinary method.
+        Function<Integer, Route> existingPet = petId -> {
+            Pet pet = pets.get(petId);
+            return (pet == null) ? reject() : complete(StatusCodes.OK, pet, Jackson.<Pet>marshaller());
+        };
+
+        // The directives here are statically imported, but you can also inherit from AllDirectives.
+        return
+                route(
+                        path("", () -> complete("Hello Pets World, visit /pet/1")),
+                        pathPrefix("pet", () ->
+                                path(INTEGER, petId -> route(
+                                        // demonstrates different ways of handling requests:
+
+                                        // 1. using a Function
+                                        get(() -> existingPet.apply(petId)),
+
+                                        // 2. using a method
+                                        put(() ->
+                                                entity(Jackson.unmarshaller(Pet.class), thePet ->
+                                                        putPetHandler(pets, thePet)
+                                                )
+                                        )
+                                ))
+                        )
+                );
+    }
+    //#unmarshall
+
+    public static void main(String[] args) throws IOException {
+        Map<Integer, Pet> pets = new ConcurrentHashMap<>();
+        Pet dog = new Pet(0, "dog");
+        Pet cat = new Pet(1, "cat");
+        pets.put(0, dog);
+        pets.put(1, cat);
+
+        final ActorSystem system = ActorSystem.create();
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        //In order to access all directives we need an instance where the routes are define.
-        HelloWorld app = new HelloWorld();
+        final ConnectHttp host = ConnectHttp.toHost("127.0.0.1", 8080);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost("localhost", 8080), materializer);
+        Http.get(system).bindAndHandle(appRoute(pets).flow(system, materializer), host, materializer);
 
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
-
-        binding
-            .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-            .thenAccept(unbound -> system.terminate()); // and shutdown when done
+        System.console().readLine("Visit http://localhost:8080/pet/1, Type RETURN to exit...");
+        system.terminate();
     }
-
-    private Route createRoute() {
-
-        return route(
-           get(() ->
-              path("", () -> complete("<h1>Hello World From Akka-Http-Json</h1>")),
-              path("json", () -> {
-                  final CompletionStage<Optional<Item>> futureMaybeItem = fetchItem("World");
-                  return onSuccess(futureMaybeItem, maybeItem ->
-                      maybeItem.map(item -> completeOK(item, Jackson.marshaller()))
-                  );
-              })
-           )
-        );
-    }
-
-    private static class Item {
-        final String id;
-        final String name;
-
-        @JsonCreator
-        Item(@JsonProperty("id") String id, @JsonProperty("name") String name) {
-            this.id = id;
-            this.name = name;
-        }
-        public String getName() { return name; }
-        public String getId() { return id; }
-    }
-
-    // (fake) async database query api
-    private CompletionStage<Optional<Item>> fetchItem(String name) {
-        return CompletableFuture.completedFuture(Optional.of(new Item("Hello", name)));
-    }
-
 }
