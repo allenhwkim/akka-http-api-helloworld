0627539a0c5e871d907d011c35f18a35341c58dc
Allen Kim <allen.kim@rci.rogers.ca>
2018-03-22 10:46:54 -0400

    updated

--- README.md
+++ README.md
@@ -20,5 +20,11 @@ $
 $ ##### Tutorial Install/Run   #####
 $ git clone https://github.com/allenhwkim/akka-http-helloworld.git
 $ cd play-helloworld
-$ sbt run
+$ sbt compile run
+$ curl http://localhost:8080/pet/1
+{"id":1,"name":"cat"}
+$ curl -X POST -H "Content-Type: application/json" -d '{"id": 2, "name": "puppy"}' http://localhost:8080/pet
+{"id":2,"name":"puppy"}
+$ curl http://localhost:8080/pet/2
+{"id":2,"name":"puppy"}
 ```
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,72 +8,48 @@ import akka.stream.ActorMaterializer;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 
 import static akka.http.javadsl.server.Directives.*;
 import static akka.http.javadsl.unmarshalling.StringUnmarshallers.INTEGER;
 
 public class HelloWorld {
 
-    //#marshall
-    private static Route putPetHandler(Map<Integer, Pet> pets, Pet thePet) {
-        pets.put(thePet.getId(), thePet);
-        return complete(StatusCodes.OK, thePet, Jackson.<Pet>marshaller());
-    }
+    public static Route appRoute() {
 
-    private static Route alternativeFuturePutPetHandler(Map<Integer, Pet> pets, Pet thePet) {
-        pets.put(thePet.getId(), thePet);
-        CompletableFuture<Pet> futurePet = CompletableFuture.supplyAsync(() -> thePet);
-        return completeOKWithFuture(futurePet, Jackson.<Pet>marshaller());
-    }
-    //#marshall
+        Map<Integer, Pet> pets = new ConcurrentHashMap<>();
+        pets.put(0, new Pet(0, "dog"));
+        pets.put(1, new Pet(1, "cat"));
 
-    //#unmarshall
-    public static Route appRoute(final Map<Integer, Pet> pets) {
-        // PetStoreController controller = new PetStoreController(pets);
-
-        // Defined as Function in order to refer to [pets], but this could also be an ordinary method.
-        Function<Integer, Route> existingPet = petId -> {
-            Pet pet = pets.get(petId);
-            return (pet == null) ? reject() : complete(StatusCodes.OK, pet, Jackson.<Pet>marshaller());
-        };
-
-        // The directives here are statically imported, but you can also inherit from AllDirectives.
         return
-        route(
-            path("", () -> complete("Hello Pets World, visit /pet/1")),
-            pathPrefix("pet", () ->
-                path(INTEGER, petId -> route(
-                    // 1. using a Function
-                    get(() -> existingPet.apply(petId)),
-
-                    // 2. using a method
-                    put(() ->
-                        entity(Jackson.unmarshaller(Pet.class), thePet ->
-                            putPetHandler(pets, thePet)
+            route(
+                path("", () -> complete("Hello Pets World, visit /pet/1")),
+                pathPrefix("pet", () ->
+                    path(INTEGER, petId -> route(
+                        get(() -> {
+                            Pet pet = pets.get(petId);
+                            return (pet == null) ? reject() : complete(StatusCodes.OK, pet, Jackson.<Pet>marshaller());
+                        })
+                    ))
+                ),
+                path("pet", () ->
+                    post(() ->
+                        entity(Jackson.unmarshaller(Pet.class), thePet -> {
+                                pets.put(thePet.getId(), thePet);
+                                return complete(StatusCodes.OK, thePet, Jackson.<Pet>marshaller());
+                            }
                         )
                     )
-                ))
-            )
-        );
+                )
+            );
     }
-    //#unmarshall
 
     public static void main(String[] args) throws IOException {
-        Map<Integer, Pet> pets = new ConcurrentHashMap<>();
-        Pet dog = new Pet(0, "dog");
-        Pet cat = new Pet(1, "cat");
-        pets.put(0, dog);
-        pets.put(1, cat);
-
         final ActorSystem system = ActorSystem.create();
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-
         final ConnectHttp host = ConnectHttp.toHost("127.0.0.1", 8080);
 
-        Http.get(system).bindAndHandle(appRoute(pets).flow(system, materializer), host, materializer);
+        Http.get(system).bindAndHandle(appRoute().flow(system, materializer), host, materializer);
 
         System.console().readLine("Visit http://localhost:8080/pet/1, Type RETURN to exit...");
         system.terminate();
